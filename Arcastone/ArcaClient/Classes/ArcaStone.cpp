afc255da6e9f5a7ca7532ca369a45a976b999a1b
김승현 <thtjdhdj10@naver.com>
2014-12-03 09:30:55 +0900

    아르카오라 추가

--- Arcastone/ArcaClient/Classes/ArcaStone.cpp
+++ Arcastone/ArcaClient/Classes/ArcaStone.cpp
@@ -17,9 +17,15 @@ void ArcaStone::initSprite()
 
 	Sprite* unitSprite;
 	unitSprite = Sprite::create("npc_arcastone.png");
+	Sprite* ora = Sprite::create("arca_ora.png");
 
 	unitSprite->setScale(HEXAGON_LENGTH*1.3 / unitSprite->getContentSize().width);
 	unitSprite->setAnchorPoint(Vec2(0.5f, 0.3f));
 
+	ora->setScale(0.5);
+	ora->setAnchorPoint(Vec2(0.48f, 0.3f));
+	ora->setOpacity(64);
+
+	m_Sprite->addChild(ora);
 	m_Sprite->addChild(unitSprite);
 }
